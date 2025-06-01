c680573286f4c0a1d36278167b51ece5d03f18ee
Vandita Goel <goelvandita777@gmail.com>
2025-06-01 14:30:03 +0530

    .

---
+++ src/config/api.example.js
@@ -0,0 +1,10 @@
+export const CoinList = (currency) =>
+  your_api_key_here;
+export const SingleCoin = (id) =>
+  your_api_key_here;
+
+export const HistoricalChart = (id, days = 365, currency) =>
+  your_api_key_here;
+
+export const TrendingCoins = (currency) =>
+    your_api_key_here;
